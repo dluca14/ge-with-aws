d987c3b7ae6191e3f8493a17a15c31a1f39da27e
divadluca14 <divadluca14@gmail.com>
2022-05-25 09:44:36 +0300

    Add some loggs and pass e message to ErrorResponse

--- app.py
+++ app.py
@@ -1,27 +1,35 @@
 import json
+import logging
 import os
 
 from great_expectations.checkpoint.types.checkpoint_result import CheckpointResult
 from great_expectations.data_context import DataContext
 
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
 
 class ErrorResponse(Exception):
     def __init__(self, message='An error occurred', status_code=500):
         self.status_code = status_code
-        self.body = json.dumps(message)
         self.headers = {
             'Content-Type': 'application/json',
         }
+        self.body = json.dumps(message)
 
     def __str__(self):
-        return {'status_code': self.status_code,
-                'body': self.body,
-                'headers': self.headers}
+        return {
+            'status_code': self.status_code,
+            'headers': self.headers,
+            'body': self.body
+        }
 
 
 def handler(event, context):
     try:
+        logger.debug('### Get data context of the validation: \r')
         data_context: DataContext = DataContext(os.getcwd() + '/project/great_expectations')
+        logger.debug('### Run checkpoint: getting-started-checkpoint \r')
         result: CheckpointResult = data_context.run_checkpoint(
             checkpoint_name="getting_started_checkpoint",
             batch_request=None,
@@ -30,7 +38,7 @@ def handler(event, context):
     except Exception as _e:
         e = _e
         if not isinstance(e, ErrorResponse):
-            e = ErrorResponse('Error while running checkpoint results')
+            e = ErrorResponse(str(e))
 
         return e.__str__()
 
